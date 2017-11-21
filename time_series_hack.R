8ea3df902533eb958758417bb74c467313889c57
jadanaque <javieradanaque@gmail.com>
2017-11-21 14:12:50 -0500

    Load data and plotting

--- time_series_hack.R
+++ time_series_hack.R
@@ -9,4 +9,22 @@ library(scales)
 library(stats)
 library(urca)
 library(dplyr)
+library(readr)
+library(lubridate)
 
+# Reading data
+train_df <- read_csv("data/TrainData.csv")
+train_df <- train_df %>%
+  mutate(Datetime = dmy_hm(Datetime))
+
+ggplot(train_df[1:2500, ], aes(Datetime, Count)) + geom_line() +
+  geom_vline(xintercept = seq(ymd_hm("2012-08-25 00:00"), ymd_hm("2012-11-30 00:00"), by = "days"), col = 2, size = 0.1, linetype = 3)
+
+
+
+
+
+
+
+
+#forcast( , h = 5112)
