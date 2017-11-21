32d5e6d88ad246e3b49cc70bbc504063a62ef81b
jadanaque <javieradanaque@gmail.com>
2017-11-21 08:51:12 -0500

    Initial commit

---
+++ time_series_hack.R
@@ -0,0 +1,12 @@
+# Script for Hackaton on Time Series Analysis (Analytics vidhya)
+# https://datahack.analyticsvidhya.com/contest/practice-problem-time-series-2/
+
+# Packages ----
+library(ggplot2)
+library(TSA)
+library(forecast)
+library(scales)
+library(stats)
+library(urca)
+library(dplyr)
+
