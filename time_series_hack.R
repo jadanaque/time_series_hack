2b30077c6ac19b72c366bf3f146efb6084ae91ce
jadanaque <javieradanaque@gmail.com>
2019-10-21 22:25:20 -0500

    Finish assignment - Hackaton - 2017

--- time_series_hack.R
+++ time_series_hack.R
@@ -35,12 +35,14 @@ grid()
 
 boxplot(traffic_ts ~ cycle(traffic_ts))
 abline(h = 106, col = 2, lty = 2)
+points(x = cycle(traffic_ts)[(length(traffic_ts)-1000):length(traffic_ts)],
+       y = traffic_ts[(length(traffic_ts)-1000):length(traffic_ts)],
+       col = 2)
 
 acf(traffic_ts, lag.max=240,
     main="ACF de la serie",
     ylab="Autocorrelaciones",
     xlab="Retardo")
-
 abline(v = 1:5, col = 2, lty = 3)
 
 pacf(traffic_ts, lag.max=240,
@@ -88,7 +90,7 @@ traffic_fit<-Arima(traffic_ts, order=c(0,1,2), seasonal = list(order = c(0,1,2),
 traffic_fit
 summary(traffic_fit)
 
-# Trying out different combinations of parameters
+# Trying out different combinations of parameters ----
 arima_pars <- list(
   list(c(0, 1, 2), c(0, 1, 2)),
   list(c(1, 1, 2), c(1, 1, 2)),
@@ -102,10 +104,15 @@ traffic_fit_l <- vector("list", length = length(arima_pars))
 results_df <- data.frame(model = as.character(arima_pars),
                          AIC = numeric(length(arima_pars)),
                          BIC = numeric(length(arima_pars)),
-                         MSE = numeric(length(arima_pars)))
+                         RMSE = numeric(length(arima_pars)))
 
 for(i in seq_along(arima_pars)){
-    #
+    traffic_fit_l[[i]] <- Arima(traffic_ts, order = arima_pars[[i]][[1]],
+                            seasonal = list(order = arima_pars[[i]][[2]], period = 24))
+    results_df[i, 2:4] <- c(traffic_fit_l[[i]][["aic"]],
+                            traffic_fit_l[[i]][["bic"]],
+                            sqrt(mean((traffic_ts - traffic_fit_l[[i]][["fitted"]])^2))
+                            )
 }
 
 
@@ -123,7 +130,55 @@ for(i in seq_along(arima_pars)){
 
 # diff with lag = 24 * 7
 
+# Split 'traffic_ts' in train-test and model building ----
+
+traffic_ts_train <- traffic_ts %>% head(13716)
+traffic_ts_test <- traffic_ts %>% tail(4572)
+
+## Trying out different combinations of parameters
+arima_pars <- list(
+  list(c(0, 1, 2), c(0, 1, 2)),
+  list(c(1, 1, 2), c(1, 1, 2))
+  # list(c(2, 1, 2), c(2, 1, 2)),
+  # list(c(2, 1, 0), c(2, 1, 0)),
+  # list(c(2, 1, 1), c(2, 1, 1))
+)
+
+traffic_fit_l <- vector("list", length = length(arima_pars))
+
+results_df <- data.frame(model = as.character(arima_pars),
+                         AIC = numeric(length(arima_pars)),
+                         BIC = numeric(length(arima_pars)),
+                         RMSE = numeric(length(arima_pars)),
+                         test_RMSE = numeric(length(arima_pars)))
+
+for(i in seq_along(arima_pars)){
+  traffic_fit_l[[i]] <- Arima(traffic_ts_train, order = arima_pars[[i]][[1]],
+                              seasonal = list(order = arima_pars[[i]][[2]], period = 24))
+  results_df[i, 2:5] <- c(traffic_fit_l[[i]][["aic"]],
+                          traffic_fit_l[[i]][["bic"]],
+                          sqrt(mean((traffic_ts_train - traffic_fit_l[[i]][["fitted"]])^2)),
+                          sqrt(mean((traffic_ts_test - forecast(traffic_fit_l[[i]], h=4572)[["mean"]])^2))
+  )
+}
+
+# best model: list(c(1, 1, 2), c(1, 1, 2))
+
+# Now, we build our final model with the full dataset: 'traffic_ts' ----
+traffic_fit_fv <- Arima(traffic_ts, order=c(1, 1, 2),
+                        seasonal = list(order = c(1, 1, 2), period=24))
+traffic_fit_fv
+summary(traffic_fit_fv)
 
 
+my_predictions <- forecast(traffic_fit_fv, h = 5112)
 
-#forcast( , h = 5112)
+my_predictions %>% summary()
+
+# Writing results in csv file
+Count <- my_predictions[["mean"]] %>% as.numeric
+
+submission_df <- data.frame(ID = 18288:23399,
+                            Count = Count)
+
+write_csv(submission_df, "submission_file.csv")
